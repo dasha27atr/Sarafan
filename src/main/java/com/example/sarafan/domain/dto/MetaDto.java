3b2f80df912fd45e74aef60c8e8743b7fdcc6d77
dasha27atr <dasha27atr@gmail.com>
2020-07-04 19:41:55 +0300

    Open Graph Protocol (link preview, youtube videos don't work)

---
+++ src/main/java/com/example/sarafan/domain/dto/MetaDto.java
@@ -0,0 +1,12 @@
+package com.example.sarafan.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MetaDto {
+    private String title;
+    private String description;
+    private String cover;
+}
